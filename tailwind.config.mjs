4aec24d082230adb7e0d01e320babfc263a6d38f
Akshay PS <psakshay2022@gmail.com>
2023-12-25 22:02:00 +0530

    add index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["League Spartan", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        night: "#020202",
+        chalk: "#FCFCFC",
+        sky: "#B2FFFD",
+        mint: "#D0FFC5",
+        primary: "#020202",
+        secondary: "#FCFCFC",
+        accent: "#B2FFFD",
+      },
+    },
+  },
+  plugins: [],
 }
